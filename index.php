dbe6e2ab6de992d38187b25f612c3cb954cf82b0
Rama Patel <me@rexsdev.com>
2017-06-12 09:24:44 -0400

    CSS & Base files

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Rama Patel - Web Developer - Systems Administrator - Student - Son</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta name="keywords" content="rama, patel, rama patel, rexs, rexs123, rexs183, rexsdev, develpment, web, design, web design, web development, system, linux, mysql, design development, app, dev, graphics, flat, material">
+		<meta name="description" content="Rama Patel - Freelance Web Developer, System administrator and College student from Mississauga, Ontario Canada">
+
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://www.rexsdev.com">
+		<meta property="og:site_name" content="Rama Patel">
+		<meta property="og:description" content="Freelance Web Developer and College student from Mississauga, Ontario Canada">
+		<meta property="og:locale" content="en-US">
+
+		<meta name="twitter:domain" content="rama-patel.com">
+		<meta name="twitter:url" content="https://www.rama-patel.com">
+		<meta name="twitter:description" content="Rama Patel - Freelance Web Developer and College student from Mississauga, Ontario Canada">
+		<meta name="twitter:site" content="@rexs183">
+
+		<meta name="robots" content="index,follow,noodp,noydir">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha256-NuCn4IvuZXdBaFKJOAcsU2Q3ZpwbdFisd5dux4jkQ5w=" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous" />
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400|Open+Sans:300,400" rel="stylesheet">
+		<link href="./assets/css/style.css" rel="stylesheet">
+		<link href="./assets/css/rama-patel.css" rel="stylesheet">
+    <link href="./assets/css/responsive.css" rel="stylesheet">
+
+		<link rel="apple-touch-icon" sizes="57x57" href="https://rexsdev.com/assets/img/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="https://rexsdev.com/assets/img/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="https://rexsdev.com/assets/img/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="https://rexsdev.com/assets/img/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="https://rexsdev.com/assets/img/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="https://rexsdev.com/assets/img/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="https://rexsdev.com/assets/img/favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="https://rexsdev.com/assets/img/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="https://rexsdev.com/assets/img/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="https://rexsdev.com/assets/img/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="https://rexsdev.com/assets/img/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="https://rexsdev.com/assets/img/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="https://rexsdev.com/assets/img/favicon/favicon-16x16.png">
+		<link rel="manifest" href="https://rexsdev.com/assets/img/favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="https://rexsdev.com/assets/img/favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	</head>
+<body>
+<!-- Header start -->
+<div id="header">
+  <!-- Container start -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h1>Hi, I'm Rama Patel.</h1>
+        <span class="sub-header-tag">Freelance Developer from Ontario, Canada</span><br>
+				<a href="#resume" class="btn btn-round hvr-bob"><i class="fa fa-angle-double-down fa-5x" aria-hidden="true"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- Container start -->
+</div>
+<!-- Header end -->
+<div id="resume">
+	<div class="container">
+		<h1 class="text-center">My R&eacute;sum&eacute;</h1>
+		<br>
+		<h2>Work Experince</h2>
+		<div class="section">
+			<div class="row">
+				<div class="col-md-3">
+					<h3>Rexs Development &amp; Design</h3>
+					<p class="timeframe">April 2014 - Present</p>
+					<a href="https://rexsdev.com">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Freelance Web Developer <b>Mississauga, Ontario Canada</b></h4>
+					<p>Developing and Designing an interactive, fluid, user friendly website. That serves the clients exact needs. Building websites / web apps to the clients exact requirements in a respectful manner and timeframe. While not only staying competitive, but also retaining a respective price that is both budget friendly and supportive for the company’s assets.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<h3>TactServ</h3>
+					<p class="timeframe">August 2013 - Present</p>
+					<a href="https://tactserv.com">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Chief Technical Officer <b>Ottawa, Ontario Canada</b></h4>
+					<p>TactServ offers many services such as Web Hosting, Game Hosting, VPS Hosting, Database Hosting and much more. My tasks at TactServ are to maintain and manage these services for the respective client. While providing the utmost respect and privacy to those who we serve. TactServ is a privately owned private hosting company. </p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<h3>Shockbyte</h3>
+					<p class="timeframe">September 2015 - Present</p>
+					<a href="https://shockbyte.com">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Remote Server Management Manager <b>Melbourne, Victoria Australia</b></h4>
+					<p>At Shockbyte my tasks range from general support to planning new innovative ideas to expand the company. My main role is the Server Support Manager. Shockbyte is based out of Melbourne in Australia. We currently offer Minecraft Hosting and Teamspeak hosting.  But are forever looking to expand the company's potential into the mass market of hosting.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<h3>Nuclearnode</h3>
+					<p class="timeframe">September 2016 - March 2017</p>
+					<a href="https://nuclearnode.com">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Web Developer <b>Danville, Virginia United States</b></h4>
+					<p>Nuclearnode is a fairly new company to the hosting market. However they provide a quality product at an exceptional price, with a wide range of services; such as Game Hosting, all the way up to but not limited to Dedicated Managed Hosting. My tasks at Nuclearnode are to maintain a respectful well presented frontend site, design and develop new web apps to help make the clients time less stressful and more seamless.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<h3>SnowStormServers</h3>
+					<p class="timeframe">March 2013 - April 2016</p>
+					<a href="https://snowstormservers.com">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Managing Director <b>Mississauga, Ontario Canada</b></h4>
+					<p>SnowStormServers was my first business venture. Stepping into the market of all forms of hosting. My daily tasks ranged very widely; general support, marketing, system upkeep and much more. Starting off with Minecraft then eventually branching out into the vast market of private cloud, until I my stepping down, and out of the company due to personal reasons. </p>
+				</div>
+			</div>
+
+		</div>
+		<h2>Education</h2>
+		<div class="section">
+			<div class="row">
+				<div class="col-md-3">
+					<h3>Mohawk College</h3>
+					<p class="timeframe">September 2016 - Present</p>
+					<a href="https://mohawkcollege.ca">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Network Engineering <b>Hamilton, Ontario Canada</b></h4>
+					<p>Mohawk College’s NESA program (Network Engineering Security Analyst) offers a wide range of skills and certifications. Which range from Comp ITA A+ cert. To Cisco’s CCNA certification. I expect to graduate with 3 semesters of CO-OP, and an masters in Network Engineering in April 2019. </p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<h3>Port Credit Secondary School</h3>
+					<p class="timeframe">September 2012 - June 2016</p>
+					<a href="http://schools.peelschools.org/sec/portcredit/Pages/default.aspx">Website</a>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-8">
+					<h4>Ontario Secondary School Diploma <b>Mississauga, Ontario Canada</b></h4>
+					<p>Your generic High school with two regional programs, SCI-tech and Strings. I was apart of the regional strings program. I graduated with my OSSD (Ontario Secondary School Diploma) and a certificate of completion for the regional strings program. I played the Viola.</p>
+				</div>
+			</div>
+		</div>
+		<h2>Skills</h2>
+		<div class="section">
+
+		</div>
+	</div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+<script async src="./assets/js/smooth-scroll.js"></script>
+</body>
+</html>
