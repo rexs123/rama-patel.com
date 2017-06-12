8e5e2972ad33fecf88b839316014efc1568391ac
Rama Patel <me@rexsdev.com>
2017-06-12 10:40:45 -0400

    Removed jQuery and redid smooth scroll
    Cleaned stuff up!

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -1,14 +1,60 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+var linksMenu = document.querySelectorAll('[href*="#"]');
+
+    for (var i = 0; i < linksMenu.length; i++) {
+
+        var linkMenu = linksMenu[i];
+
+        linkMenu.addEventListener("click", function(event) {
+
+
+            var positionInicial = window.scrollY;
+
+            var targetHref = this.getAttribute('href');
+
+            if (targetHref !== '#') {
+
+                var target = document.querySelector(targetHref);
+                var positionTarget = target.offsetTop;
+
+                var more = positionTarget > positionInicial;
+
+                event.preventDefault();
+
+                    if (typeof InstallTrigger !== 'undefined') {
+                        var velocidade = 10;
+                        var timer = 10;
+                    }else{
+                        var velocidade = 100;
+                        var timer = 1;
+                    }
+                    var scroll = setInterval(function() {
+                        var position = window.scrollY;
+
+                        var stop = position == positionTarget;
+
+                        if(more){
+                          var direcao = positionTarget - position;
+                        }else{
+                          var direcao = position - positionTarget;
+                        }
+                        var parte = Math.ceil( direcao / velocidade);
+                        if(more){
+                          var smooth = position + parte;
+                        }else{
+                          var smooth = position - parte;
+                        }
+                        if (stop) {
+                            clearInterval(scroll);                                             } else {
+                            window.scrollTo(0, smooth);
+                        }
+                        }, timer);
+
+                for (var i = 0; i < scroll; i++) {
+                    window.clearInterval(i);
+                    window.addEventListener('wheel', function() {
+                        window.clearInterval(i);
+                    });
+                }
+            }
+        });
     }
-  });
-});
